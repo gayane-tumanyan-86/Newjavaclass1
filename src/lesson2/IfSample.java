49c6074f80240228790768b45afb4e3d8e037486
gayane-tumanyan-86 <tumanjan.gug@mail.ru>
2021-10-04 00:57:11 +0400

    false fails

---
+++ src/lesson2/IfSample.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+public class IfSample {
+    public static void main(String[] args) {
+        int x, y;
+        x = 10;
+        y = 20;
+        if (x < y) System.out.println("x меньше y");
+        x=x*2;
+        if(x==y) System.out.println("x теперь равноy");
+   x=x*2;
+   if (x>y) System.out.println ("x теперь больше у");
+
+    }
+}
